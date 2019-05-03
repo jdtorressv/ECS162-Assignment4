f5db7b1aecc87ce43ddfb67c39ed767f37b07fad
Joseph Torres <jdtorres@ucdavis.edu>
2019-05-02 21:53:47 -0700

    Project Complete!

--- hello/miniServer3.js
+++ hello/miniServer3.js
@@ -5,8 +5,11 @@ const port = 57443 // you need to put your port number here
 function queryHandler(req, res, next) {
     let url = req.url;
     let qObj = req.query;
-    console.log(qObj);
-    if (qObj.animal != undefined) {
+    if (qObj.word != undefined) {
+	let flipped = qObj.word.split("").reverse().join("");
+    	res.json({"palindrome" : qObj.word + flipped}); 	
+    } 
+    else if (qObj.animal != undefined) {
 	res.json( {"beast" : qObj.animal} );
     }
     else {
--- hello/public/palindrome.js
+++ hello/public/palindrome.js
@@ -1,7 +1,15 @@
-let url = “query?animal=bat"
+function run() {
+let input = document.getElementById("word").value;
+let url = "query?word=" + input;
 let xhr = new XMLHttpRequest();
-xhr.open(method, url, true);
-xhr.onload = function() {…};
-xhr.onerror = function() {…};
+xhr.open("GET", url, true);
+xhr.onload = function() {
+	res = xhr.responseText;
+	document.getElementById('outputGoesHere').textContent = res;
+};
+xhr.onerror = function() {
+	console.log("An error occurred");
+};
 xhr.send();
 
+}
